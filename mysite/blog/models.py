85039d994933538ea04d20a61043d51708361d2d
Crystal <jpkoq18@gmail.com>
2022-06-14 22:16:31 +0800

    新增一個model留言板

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+
 class Post(models.Model):
 
     STATUS_CHOICES = (
@@ -28,3 +30,37 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    username = models.CharField(max_length=120)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.username,self.post)
+    
