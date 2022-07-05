81171a57aa7773deaa32660fc5717248e3d879e7
Crystal <jpkoq18@gmail.com>
2022-07-05 20:56:39 +0800

    美化部落格

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -9,12 +11,12 @@ from taggit.managers import TaggableManager
 class Post(models.Model):
 
     STATUS_CHOICES = (
-        ('draft','Draft'),
-        ('published' ,'Published')
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
-    slug=models.SlugField(max_length=250,
-                        unique_for_date='publish')
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -28,7 +30,6 @@ class Post(models.Model):
         choices=STATUS_CHOICES,
         default='draft')
     tags = TaggableManager()
-
 
     class Meta:
         ordering = ('-publish',)
@@ -49,6 +50,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
@@ -57,17 +59,16 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ('-created',)
 
     def __str__(self):
-        return 'Comment by {} on {}'.format(self.username,self.post)
+        return 'Comment by {} on {}'.format(self.username, self.post)
 
     def avatar(self, size=60):
         md5_digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             md5_digest, size)
-    
